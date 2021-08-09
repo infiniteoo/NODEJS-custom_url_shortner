1bf0b04cdc26f9aae6b798843e08461da447a591
Troy Dorman <troydorman@gmail.com>
2021-08-08 21:26:30 -0600

    successfully connected to our mongodb cloud database

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
-require("dotenv").config();
+const connectDB = require("./config/db");
 
 const app = express();
+connectDB();
 
 app.use(express.json({ extended: false }));
 
